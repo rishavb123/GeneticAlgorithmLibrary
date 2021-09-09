4b8f97f6a84ad6c6f2268a34d57f7ac1fbb534d3
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-09-08 23:36:11 -0400

    minor changes

--- dna.py
+++ dna.py
@@ -1,5 +1,9 @@
 class DNA:
 
+    def get_params(self):
+        print("Using default get_params function. Please override this function for the algorithm to work.")
+        return self
+
     def fitness(self):
         print("Using default fitness function. Please override this function for the algorithm to work.")
         return 0
